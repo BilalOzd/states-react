c239b9ae6da850f0552403e473e420363549aa43
Bilal Ozdemir <20190301034@dogus.edu.tr>
2024-01-09 00:00:08 +0300

    setState - Object definition.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,35 @@ function App() {
   const [name, setName] = useState("Bilal")
   const [age, setAge] = useState(22)
   const [brands , setBrands] = useState(["Koenigsegg", "McLaren", "RAM"])
+  const [carParts, setCarParts] = useState({spoilerBrand: "Aisin", wheels: "sport", NOdoors: 2})
 
   console.log(brands);
+  console.log(carParts);
 
   return (
     <div className="App">
       
-      <h2>{name} {age}</h2>
+        <h2>{name} {age}</h2>
 
-      <button onClick = {() => setName("Karahanlı")}>CHANGE NAME</button>
-      <br></br>
-      <button onClick = {() => setAge(29)}>CHANGE AGE</button>
-      <br></br>      <hr></hr>
+        <button onClick = {() => setName("Ayça")}>CHANGE NAME</button>
+        <br></br>
+        <button onClick = {() => setAge(29)}>CHANGE AGE</button>
+        <br></br>      <hr></hr>
 
-      <h2>{brands.map(
-        (brand, key) =>
-          <div key={key}>{brand}</div>
-      )}</h2>
-      <button onClick = {() => setBrands([...brands, "Lamborghini"])}>ADD BRAND</button>
+        <h2>{brands.map(
+          (brand, key) =>
+            <div key={key}>{brand}</div>
+        )}</h2>
+        <button onClick = {() => setBrands([...brands, "Lamborghini"])}>ADD BRAND</button>
+        <br></br>      <hr></hr>
+
+        <h2>
+         {carParts.spoilerBrand} {carParts.NOdoors}
+        </h2>
+        <div>
+        <button onClick = {() => setCarParts({...carParts, spoilerBrand: "xDrive"})}>CHANGE spoilerBrand</button>
+        <button onClick = {() => setCarParts({...carParts, NOdoors: 4})}>CHANGE NOdoors</button>
+        </div>
 
     </div>
   );
